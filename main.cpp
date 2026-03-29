d8d26d6eb7d5b151b4f3a8b205549761a47ec9cd
Gayan-98 <dongayanpathirage@gmail.com>
2026-03-29 12:25:36 +0530

    data types with I/O

--- main.cpp
+++ main.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 
 int main(){
+
+    string name;
+    int age;
     cout << "Hello world";
+    
+    cout <<"enter your name : ";
+    cin >> name;
+
+    cout <<"enter your age: ";
+    cin >> age;
+    
+
+    cout<< "your name is : " << name;
+    cout << "your age is : " << age;
+
+
+
     return 0;
+
 }
